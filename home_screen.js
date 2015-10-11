934d0991ac12a672b24a84e4f2c88e87f89e1e80
isabellamercado <mus310@hacarcsales130.hac.hafele.corp>
2015-10-11 18:36:56 -0400

    hiding main menu

--- home_screen.js
+++ home_screen.js
@@ -1,5 +1,5 @@
-var people_button= document.getElementByID("people")
-var main_menu =document.getElementByID("menu")
+var people_button= document.getElementById("people")
+var main_menu=document.getElementById("menu")
 var people_screen =document.getElementById("people_screen")
 
 people_button.addEventListener("click", function(){
