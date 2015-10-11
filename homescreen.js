1592e1510a4b95fd8e78f91a49e817a6a476df73
shibangsisaha <shibangisaha@gmail.com>
2015-10-11 18:03:15 -0400

    Added some Java Script?

--- homescreen.js
+++ homescreen.js
@@ -1,1 +1,10 @@
-alert("I AM ANNOYING")
+var people_button = document.getElementById("people")
+var main_menu= document.getElementById("menu"
+var people_screen= document.getElementById("people_screen")
+
+people_button.addEventListener("click", function() {
+    main_menu.style.display = "none"
+    people_screen.style.display = "block"
+
+
+})
