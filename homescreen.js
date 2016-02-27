17d1226571eda29b55c313a6ff66c7a9e2cf40fd
shibangsisaha <shibangisaha@gmail.com>
2016-02-27 13:58:25 -0500

    Updated Pics

--- homescreen.js
+++ homescreen.js
@@ -2,8 +2,6 @@ var people_button = document.getElementById("people")
 var main_menu= document.getElementById("menu")
 var people_screen= document.getElementById("people_screen")
 var back= document.getElementById("back")
-var shibangi= document.getElementById("shibangi")
-var liliana= document.getElementById("liliana")
 var tap_to_show= document.getElementById("tap to show")
 
 people_button.addEventListener("click", function() {
@@ -16,5 +14,5 @@ back.addEventListener("click", function() {
     main_menu.style.display = "block"
     shibangi.style.display = "none"
     liliana.style.display = "none"
-    
+
   })
