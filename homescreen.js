543f1e31c7424b25b16f92c52e7100a5fe7f4d48
shibangsisaha <shibangisaha@gmail.com>
2016-02-27 13:21:04 -0500

    Renamed home screen

---
+++ homescreen.js
@@ -0,0 +1,20 @@
+var people_button = document.getElementById("people")
+var main_menu= document.getElementById("menu")
+var people_screen= document.getElementById("people_screen")
+var back= document.getElementById("back")
+var shibangi= document.getElementById("shibangi")
+var liliana= document.getElementById("liliana")
+var tap_to_show= document.getElementById("tap to show")
+
+people_button.addEventListener("click", function() {
+    main_menu.style.display = "none"
+    people_button.style.display= "none"
+    people_screen.style.display = "block"
+  })
+
+back.addEventListener("click", function() {
+    main_menu.style.display = "block"
+    shibangi.style.display = "none"
+    liliana.style.display = "none"
+    
+  })
