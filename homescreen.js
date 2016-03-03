8b4c00f48f72635aad2090d7c9e8ccd209931eeb
Milan Iliev <milaniliev@gmail.com>
2016-03-03 14:36:13 -0500

    Some more corrections

--- homescreen.js
+++ homescreen.js
@@ -7,18 +7,25 @@ var things= document.getElementById ("things")
 
 things.addEventListener("click", function (){
   main_menu.style.display= "none"
-  things.style.display= "none"
+  things.style.display= "block"
 })
 
 people_button.addEventListener("click", function() {
-    main_menu.style.display = "none"
-    people_button.style.display= "none"
-    people_screen.style.display = "block"
-  })
+  main_menu.style.display = "none"
+  people_screen.style.display = "block"
+})
 
 back.addEventListener("click", function() {
-    main_menu.style.display = "block"
-    shibangi.style.display = "none"
-    liliana.style.display = "none"
+  main_menu.style.display = "block"
+  people_screen.style.display="none"
+})
 
-  })
+var people = new CollectionManager({
+  name: 'people',
+  fields: ['name', 'title'],
+  photo: true,
+  list: document.querySelector('#people_screen'),
+  new_form: document.querySelector("#add_person")
+})
+
+people.restore()
