8b4545d97bcff8c09a245e65b3d14c35e836f55a
Milan Iliev <milaniliev@gmail.com>
2016-03-06 01:38:01 -0500

    Compiled

--- collection_manager.js
+++ collection_manager.js
@@ -1,52 +1,68 @@
 'use strict';
 
-var _createClass = function () {
-  function defineProperties(target, props) {
-    for (var i = 0; i < props.length; i++) {
-      var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);
-    }
-  }return function (Constructor, protoProps, staticProps) {
-    if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;
-  };
-}();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-}
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var CollectionManager = function () {
   function CollectionManager(config) {
     _classCallCheck(this, CollectionManager);
 
     this.objects = [];
-    this.list = new Vue({ el: config.list, data: { objects: this.objects } });
+    if (config.display) {
+      this.object_display = config.display;
+      this.object_display.addEventListener('click', function () {
+        this.object_display.style.display = 'none';
+      }.bind(this));
+    }
+    this.list = new Vue({
+      el: config.list,
+      data: { objects: this.objects },
+      methods: {
+        display: function (event) {
+          event.target.classList.toggle('display');
+          event.target.parentNode.classList.toggle('display');
+        }.bind(this)
+      }
+    });
     this.name = config.name;
     var new_object = {};
     config.fields.forEach(function (property) {
-      new_object[property] = property;
+      new_object[property] = null;
     });
     var manager = this;
 
-    this.form = new Vue({ el: config.new_form, data: new_object,
+    this.add = function (event) {
+      event.preventDefault();
+      if (config.photo) {
+        var reader = new FileReader();
+        reader.readAsDataURL(this.$el.querySelector('input[type=file]').files[0]);
+        reader.addEventListener("load", function () {
+          var new_object = {};
+          config.fields.forEach(function (property) {
+            new_object[property] = this[property];
+          }.bind(this));
+          new_object.photo = reader.result;
+          manager.objects.push(new_object);
+          localStorage.setItem(config.name, JSON.stringify(manager.objects));
+          alert((new_object.name || 'Object') + ' added');
+        }.bind(this), false);
+      } else {
+        var new_object = {};
+        config.fields.forEach(function (property) {
+          new_object[property] = this[property];
+        }.bind(this));
+        manager.objects.push(new_object);
+        localStorage.setItem(config.name, JSON.stringify(manager.objects));
+        alert((new_object.name || 'Object') + ' added');
+      }
+    };
+
+    this.form = new Vue({
+      el: config.new_form,
+      data: new_object,
       methods: {
-        add: function add(event) {
-          event.preventDefault();
-          var reader = new FileReader();
-          reader.readAsDataURL(this.$el.querySelector('input[type=file]').files[0]);
-          reader.addEventListener("load", function () {
-            var new_object = {};
-            config.fields.forEach(function (property) {
-              new_object[property] = this[property];
-            }.bind(this));
-            if (config.photo) {
-              new_object.photo = reader.result;
-            }
-            manager.objects.push(new_object);
-            localStorage.setItem(config.name, JSON.stringify(manager.objects));
-          }.bind(this), false);
-        }
+        add: this.add
       }
     });
   }
